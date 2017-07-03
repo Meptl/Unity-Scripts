78267b88ee5ecab41e9b265a498ad22b1dcdd1ed
Christopher Chin <ctchin13@gmail.com>
2017-07-03 03:40:29 -0400

    Slope movement using plane projection.

--- BasicMovement.cs
+++ BasicMovement.cs
@@ -8,169 +8,175 @@ using System.Collections;
 
 [RequireComponent (typeof (CharacterController))]
 public class BasicMovement : MonoBehaviour {
-    public float mass = 5f;
     public float moveSpeed = 6f;
     public float moveSide = 0.86f;
     public float moveBack = 0.7f;
     public float jumpControlPenalty = 0.05f; // movement speed penalty while in air.
-    public float jumpForce = 8f;
+    public float jumpForce = 12f;
     public float jumpDelay = 0.3f; // Delay between being able to jump
-    public float gravity = 3f;
+    public float gravityModifier = 0.5f;
 
-    // normalForce: prevents users from sticking to walls by adding force towards them
+    // normalForce: prevents users from sticking to walls by adding force away from
     public float normalForce = 0.025f;
-	public float rayLength = 0.3f;
 
-	private const float baseGrav = 0.1f;
+    // baseGravStr applied when on ground
+    private const float gravityBaseStr = -0.1f;
+    private const float groundCheckLength = 0.5f;
     private CharacterController controller;
-	private Vector3 movementVec;
-	private Vector3 groundNorm;
-	private bool canJump;
-	private float accumGravity;
+    private Vector3 movementVec, slopeWallForce;
+    private Vector3 gravityAccum, gravityBase;
+    private bool canJump;
 
-	// User Input
-	private float hori, vert;
-	private bool jumpKey;
+    // User Input
+    private float hori, vert;
+    private bool jumpKey;
 
-	private Ray groundRay;
+    private Ray groundRay;
 
     void Awake()
     {
-		movementVec = Vector3.zero;
-		groundNorm = Vector3.up;
-		canJump = true;
-		groundRay = new Ray(transform.position, Vector3.down); // origin is wrong
-	}
+        movementVec = Vector3.zero;
+        canJump = true;
+        groundRay = new Ray(transform.position, Vector3.down); // origin is wrong
+        gravityBase = new Vector3(0, gravityBaseStr, 0);
+    }
 
-	void Start()
-	{
+    void Start()
+    {
         controller = GetComponent<CharacterController>();
-	}
+    }
 
     void Update()
-	{
-		// We use raw input to have a more responsive movement
-		hori = Input.GetAxisRaw("Horizontal"); // Only evaluates to -1, 0, or 1
-		vert = Input.GetAxisRaw("Vertical"); // Only evaluates to -1, 0, or 1
-		jumpKey = Input.GetButton("Jump");
-	}
+    {
+        // We use raw input to have a more responsive movement
+        hori = Input.GetAxisRaw("Horizontal"); // Only evaluates to -1, 0, or 1
+        vert = Input.GetAxisRaw("Vertical"); // Only evaluates to -1, 0, or 1
+        jumpKey = Input.GetButton("Jump");
+    }
 
-	void FixedUpdate()
-	{
-		snapToGround();
-
+    void FixedUpdate()
+    {
         bool grounded = controller.isGrounded;
-		Debug.Log(grounded.ToString());
 
-        Vector3 inputVec = createInputVector(hori, vert, grounded);
-		if (grounded) {
-			movementVec = inputVec;
-			if (jumpKey && canJump) {
-				StartCoroutine(applyJump());
-			}
-		} else {
-			// This may cause really fast air movements over time
-			// We'll see.
-			movementVec += inputVec;
-		}
+        Vector3 inputVec = createInputVector(hori, vert);
 
-		applyGravity(grounded);
-		DebugUtilities.debugVector(transform.position + new Vector3(0, -1, 0), movementVec.normalized);
+        if (grounded) {
+            RaycastHit hitInfo;
+            if (groundCheck(out hitInfo)) {
+            } else {
+                //Debug.Log("ground check while grounded failed.");
+            }
+
+            // Only project if going downhill. There are stuttering issues going uphill.
+            Vector3 groundNorm = getGroundNormal();
+            if (groundNorm != Vector3.right && inputVec != Vector3.zero && Vector3.Dot(groundNorm, inputVec) >= 0) {
+                inputVec = Vector3.ProjectOnPlane(inputVec, groundNorm);
+            }
+
+            movementVec = inputVec;
+            if (jumpKey && canJump) {
+                movementVec += Vector3.up * jumpForce;
+                StartCoroutine(startJumpDelay());
+            }
+        } else {
+            // This may cause really fast air movements over time, we'll see.
+            inputVec *= jumpControlPenalty;
+            movementVec += inputVec;
+        }
+
+        applyGravity(ref movementVec, grounded);
+
+        //movementVec += slopeWallForce;
+        //slopeWallForce = Vector3.zero;
+
+        Vector3 debugVec = Vector3.ClampMagnitude(movementVec, 1f);
+        DebugUtilities.debugVector(transform.position + new Vector3(0, -1, 0), debugVec);
         controller.Move(movementVec * Time.deltaTime);
     }
 
-	/**
-	 * Sends a raycast downward. If it hits, move downward
-	 * Used to allow smooth downward traversal of slopes
-	 */
-	private void snapToGround() {
-		// Make sure we didn't recently just jump by checking the canJump flag.
-		if (!controller.isGrounded) {
-			Vector3 controllerBase = transform.position - new Vector3(0, controller.height / 2, 0);
-			groundRay.origin = controllerBase;
+    /**
+     * Raycasts toward the ground from the base of the controller.
+     */
+    private bool groundCheck(out RaycastHit hitInfo) {
+        Vector3 controllerBase = transform.position - new Vector3(0, controller.height / 2, 0);
+        groundRay.origin = controllerBase;
 
-			RaycastHit hit;
-			if (Physics.Raycast(groundRay, out hit, rayLength)) {
-				// Don't snap for two reasons: 
-				// 1. We've recently jumped.
-				// 2. We have a velocity going nearly down which indicates we're
-				//    going to hit the ground (likely not going down a slope)
-				if (canJump) {
-					Vector3 delta = hit.point - controllerBase;
-					Debug.Log("Snapping!");
-					controller.Move(delta);
-				}
-			}
-		}
-	}
+        RaycastHit hit;
+        if (Physics.Raycast(groundRay, out hit, groundCheckLength)) {
+            hitInfo = hit;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Ray casts below the controller returning the hit normal or Vector3.right
+     */
+    private Vector3 getGroundNormal() {
+        Vector3 controllerBase = transform.position - new Vector3(0, controller.height / 2, 0);
+        groundRay.origin = controllerBase;
+
+        RaycastHit hit;
+        if (Physics.Raycast(groundRay, out hit, groundCheckLength)) {
+            return hit.normal;
+        }
+        return Vector3.right;
+    }
+
+    /**
+     * Given the ground point, moves controller toward it.
+     */
+    private void snapToGround(Vector3 point) {
+        // Don't snap if:
+        // 1. We've recently jumped.
+        if (canJump) {
+            Vector3 controllerBase = transform.position - new Vector3(0, controller.height / 2, 0);
+            Vector3 delta = point - controllerBase;
+            controller.Move(delta);
+        }
+    }
 
     /**
      * When controller is standing on a slope larger than the slopeLimit
- 	 * apply a normal force. Side collisions are checked since walls give a
+     * apply a normal force. Side collisions are checked since walls give a
      * slopeAngle larger than the slope limit (90). Ignore ceiling collisions.
      */
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         // Causes movements that hit a ceiling to stop early.
         if ((controller.collisionFlags & CollisionFlags.Above) != 0 && movementVec.y > 0) {
-			movementVec.y = 0;
+            movementVec.y = 0;
         }
 
-		// Applies a normal force from anything above the slope limit of the charController.
-		/*
-        if (hit.normal.y > 0 && (controller.collisionFlags & CollisionFlags.Sides) == 0) {
-            // slopeAngle for flat surface is 90. Will translate this later.
-            float slopeAngle = Vector3.Angle(hit.normal, transform.right);
-            if (slopeAngle > 90) {
-                // 92 degrees should translate to 88 degrees
-                slopeAngle -= 90;
-                slopeAngle = 90 - slopeAngle;
-            }
-
-            // inverse one last time to get true angle of a slope.
-            slopeAngle = 90 - slopeAngle;
-
+        if ((controller.collisionFlags & CollisionFlags.Below) != 0) {
+            float slopeAngle = calcSlopeAngle(hit.normal);
             if (slopeAngle > controller.slopeLimit) {
-				//Debug.Log("This happened!");
-                // Only horizontal expulsion.
-                Vector3 expulsionVec = hit.normal;
-                expulsionVec.y = 0;
+                // Remove movementVec towards the wall
+                slopeWallForce = hit.normal * normalForce;
             }
         }
-		*/
-
-		if ((controller.collisionFlags & CollisionFlags.Below) != 0) {
-			float slopeAngle = calcSlopeAngle(hit.normal);
-			groundNorm = hit.normal;
-
-			if (slopeAngle > controller.slopeLimit) {
-			} else {
-			}
-		}
-
-
-		//snapToGround(hit);
     }
 
-	/**
-	 * Returns the angle of a slope that a CharController hit given the slope's normal.
-	 */
-	private float calcSlopeAngle(Vector3 normal)
-	{
-		Vector3 flatNormal = normal;
-		flatNormal.y = 0;
-
-		float slopeAngle = Vector3.Angle(normal, flatNormal);
-		slopeAngle = Mathf.Abs(slopeAngle - 90);
-
-		return slopeAngle;
-	}
-	
     /**
-     * Create the flat movement vector resulting from: user input, constant modification
+     * Returns the angle of a slope that a CharController hit given the slope's normal.
+     */
+    private float calcSlopeAngle(Vector3 normal)
+    {
+        Vector3 flatNormal = normal;
+        flatNormal.y = 0;
+
+        float slopeAngle = Vector3.Angle(normal, flatNormal);
+        slopeAngle = Mathf.Abs(slopeAngle - 90);
+
+        return slopeAngle;
+    }
+
+    /**
+     * Create the movement vector resulting from: user input, constant modification
      * from moving forward/backward, ground state
      */
-    private Vector3 createInputVector(float hori, float vert, bool grounded)
+    private Vector3 createInputVector(float hori, float vert)
     {
         Vector3 movement = new Vector3(hori, 0, vert);
 
@@ -184,10 +190,6 @@ public class BasicMovement : MonoBehaviour {
 
         movement *= moveSpeed;
 
-		if (!grounded) {
-            movement *= jumpControlPenalty;
-        }
-
         return movement;
     }
 
@@ -195,26 +197,25 @@ public class BasicMovement : MonoBehaviour {
      * Coroutine implements jumping and delay between jumps.
      * Modifies class variable movementVec
      */
-	private IEnumerator applyJump()
+    private IEnumerator startJumpDelay()
     {
-		movementVec += Vector3.up * jumpForce;
-
         canJump = false;
         yield return new WaitForSeconds(jumpDelay);
         canJump = true;
     }
 
-	/**
-	 * Adds gravity to movementVec, modifiying accumGravity if needed.
-	 */
-	private void applyGravity(bool grounded) {
-		if (grounded) {
-			// Applying 0 downward force on the controller prevents the grounded flag from flipping
-			accumGravity = baseGrav;
-		} else {
-			accumGravity += gravity * Time.deltaTime;
-		}
+    /**
+     * Adds gravity to movementVec, modifiying accumGravity if needed.
+     */
+    private void applyGravity(ref Vector3 vec, bool grounded) {
+        if (grounded) {
+            // Applying 0 downward force on the controller prevents the grounded flag from flipping
+            gravityAccum = gravityBase;
+        } else {
+            // Physics.gravity is already negative.
+            gravityAccum += gravityModifier * Physics.gravity * Time.deltaTime;
+        }
 
-		movementVec += Vector3.down * accumGravity;
-	}
+        vec += gravityAccum;
+    }
 }
